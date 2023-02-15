eef545df102c6abfbbe97b3dbe47928fc01ced48
mhertovm <mhertovmasyan904@gmail.com>
2023-02-15 14:05:50 +0400

    box

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header.js';
+import New from './components/New.js';
+
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <Header />
+      <section className="App-section">
+      <New />
+      </section>
+      <footer className="App-footer">
+
+      </footer>
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import './Header.css';
+
+function Header() {
+    return (
+        <header>
+            <img className="logo" src="img/logo.png" alt="Logo" />
+            <nav className="navBar">
+                
+            </nav>
+        </header>
+    );
+}
+  
+  export default Header;
